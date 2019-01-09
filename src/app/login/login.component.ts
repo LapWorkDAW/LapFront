027906ba37ab47876e4585d949ef023e0702c9ea
Mireia <mireiacolomerr@gmail.com>
2019-01-09 13:16:01 +0100

    login created, just basic, no css

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./app.css', './login.css']
+})
+
+export class LogInComponent { }
