2b6ea92022338ce4fed78466af0646cf0e5e000a
Mireia <mireiacolomerr@gmail.com>
2019-01-11 12:51:42 +0100

    yuliya's commit css fixed problems

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'footer',
     templateUrl: './footer.component.html',
-    styleUrls: ['./footer.css']
+    styleUrls: ['./footer.component.css']
 })
 export class FooterComponent { 
     
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'register',
     templateUrl: './register.component.html',
-    styleUrls: ['./register.css']
+    styleUrls: ['../home/home.component.css', './register.component.css']
 })
 
 export class RegisterComponent { }
