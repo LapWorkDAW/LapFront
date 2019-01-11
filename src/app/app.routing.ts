cac9abd0d6b703638a6347752488b79bfd5a02c2
Yuliya Kobruseva <dafna_89@bk.ru>
2019-01-11 13:06:14 +0100

    He implementado resto del Routing y footer en pagina home

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,16 +1,18 @@
-import {ModuleWithProviders} from '@angular/core';
-import {Routes,RouterModule} from '@angular/router';
-import { NgModule }             from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './login/login.component';
 
-const appRoutes:Routes=[
-    {path:"", component:HomeComponent}, 
-    {path:'register',component:RegisterComponent}    
+const appRoutes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LogInComponent }
 ];
 
 @NgModule({
-    imports: [ RouterModule.forRoot(appRoutes) ],
-    exports: [ RouterModule ]
-  })
-  export class AppRoutingModule {}
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
