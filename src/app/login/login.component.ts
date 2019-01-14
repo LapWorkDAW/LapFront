c75093216868a82650386afde3a3ee7f88cf8a4e
Mireia <mireiacolomerr@gmail.com>
2019-01-14 12:25:51 +0100

    file error fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'login',
     templateUrl: './login.component.html',
-    styleUrls: ['../home/home.component.css', './login.css']
+    styleUrls: ['../home/home.component.css', './login.component.css']
 })
 
 export class LogInComponent { }
