8bd05b6ab8cd47fd15316214105ca142341b6e00
Mireia <mireiacolomerr@gmail.com>
2019-01-14 12:19:48 +0100

    css files added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'login',
     templateUrl: './login.component.html',
-    styleUrls: []
+    styleUrls: ['../home/home.component.css', './login.css']
 })
 
 export class LogInComponent { }
