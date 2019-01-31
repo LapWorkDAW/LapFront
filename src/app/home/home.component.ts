bdd0bd3ece5bde6859df3731ce5782ff6ae8c855
MireiaColomer <mireiacolomerr@gmail.com>
2019-01-31 12:25:11 +0100

    pagina de contacto, half way done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,39 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class HomeComponent {
-  projects = [{
-    idProject: "1",
-    creator: "Mireia Colomer",
-    name: "Marver Character Creation",
-    desc: "This project consists in the creation for a movie.",
-    type: "Tech",
-    dateStart: "10/12/2018",
-    dateFinish: "29/06/2018",
-    img: "",
-    projectStatus: "0"
-    /* likes:"" projectstatus 1 acabat 0no acabat */
-  }, {
-    idProject: "2",
-    creator: "Mireia Colomer",
-    name: "Framework Creation",
-    desc: "This project consists in the creation for our webpage framework",
-    type: "Tech",
-    dateStart: "10/12/2018",
-    dateFinish: "29/06/2018",
-    img: "",
-    projectStatus: "1"
-    /* likes:"" */
-  }];
-  project = {
-    idProject: "",
-    creator: "",
-    name: "",
-    desc: "",
-    type: "",
-    dateStart: "",
-    dateFinish: "",
-    img: "",
-    projectStatus: ""
-    /* likes:"" */
-  };
+
 }
