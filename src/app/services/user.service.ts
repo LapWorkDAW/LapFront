0c3f8b8dd1a5c88566c8fd89749014f7bab17a59
uriigrao <oriolgm95@gmail.com>
2019-02-12 11:52:16 +0100

    change in user.service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,19 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from 'src/assets/models/User';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { User } from "src/assets/models/User";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UserService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   postUser(user: User): Observable<any> {
     let url = "";
-    return this.http.post(
-      url, user, { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
-    );
+    return this.http.post(url, user, {
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
+    });
+  }
+  getAllUsers(fn: Function) {
+    let url =
+      "http://localhost/LapBack/api.php?controller=User&function=getactiv";
+    fetch(url)
+      .then(resp => resp.json())
+      .then(resp => {
+        fn(resp.data);
+      });
   }
 }
