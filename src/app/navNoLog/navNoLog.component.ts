4958552bc471fb331f0d44c9431997a36ddc0628
Mireia Colomer <mireiacolomerr@gmail.com>
2019-03-12 13:05:54 +0100

    nav bootstrap + bootstrap implementation 4.0

--- src/app/navNoLog/navNoLog.component.ts
+++ src/app/navNoLog/navNoLog.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from 'angular-6-social-login';
 import { Router } from '@angular/router';
 
@@ -9,22 +9,22 @@ import { Router } from '@angular/router';
 })
 
 export class NavNoLogComponent {
-    isSignIn:boolean=false;
-    token:String;
+    isSignIn: boolean = false;
+    token: String;
     constructor(private socialAuthService: AuthService, public router: Router) { }
     public signOut() {
         this.socialAuthService.signOut();
         this.router.navigate(['/']);
         console.log("user logOut");
-        localStorage.setItem('token',"null");
+        localStorage.setItem('token', "null");
 
     }
 
     ngOnInit() {
         this.token = localStorage.getItem('token');
         if (this.token != "null") {
-            this.isSignIn=true;
+            this.isSignIn = true;
             console.log(this.isSignIn);
-        } 
+        }
     }
 }
