bfbcac3e197744e6bee7df281d43f22d2ca02277
MireiaColomer <mireiacolomerr@gmail.com>
2019-03-19 13:10:55 +0100

    profile current user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {AuthenticationService} from './services/authentication.service';
-import {User} from 'src/assets/models/User';
+import { AuthenticationService } from './services/authentication.service';
+import { User } from 'src/assets/models/User';
 import { from } from 'rxjs';
 
- @Component({
+@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  }
+}
