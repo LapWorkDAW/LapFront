ef8e5d2458b9ccb13a4b613637ad25e693bd8bb9
MireiaColomer <mireiacolomerr@gmail.com>
2019-03-29 11:40:02 +0100

    img bugs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/assets/models/User';
+import { Project } from 'src/assets/models/Project';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'home',
@@ -9,8 +11,33 @@ import { User } from 'src/assets/models/User';
 
 export class HomeComponent implements OnInit {
   currentUser: User;
+  projectsInProgress: Array<Project>;
+  projectsFinished: Array<Project>;
+
+  constructor(private projectService: ProjectService) { }
   ngOnInit(): void {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+  getProjectsInProgress() {
+    this.projectService.getProjectNoFinished(this.currentUser.token).subscribe(
+      result => {
+        this.projectsInProgress = result["data"];
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  getProjectsFinished() {
+    this.projectService.getProjectFinished(this.currentUser.token).subscribe(
+      result => {
+        this.projectsFinished = result["data"];
+      },
+      error => {
+        console.log(error);
+      }
+    )
   }
 
 }
