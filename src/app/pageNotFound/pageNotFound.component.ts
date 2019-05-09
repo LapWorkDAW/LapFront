d945dcc1933b23036f9a345a7682ed225043e25c
MireiaColomer <mireiacolomerr@gmail.com>
2019-05-09 12:38:43 +0200

    page 404 created

--- src/app/pageNotFound/pageNotFound.component.ts
+++ src/app/pageNotFound/pageNotFound.component.ts
@@ -2,7 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'pageNotFound',
-    templateUrl: './pageNotFound.component.html'
+    templateUrl: './pageNotFound.component.html',
+    styles: [`
+    body{
+        background: #343a40;
+    }`]/* ,
+    styleUrls: ['./pageNotFound.component.css'] */
 })
 export class PageNotFoundComponent {
 
